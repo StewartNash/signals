ad3d0c55f4efce79976c4944ab93f72a70dff381
StewartNash <Stewart.M.Nash@gmail.com>
2025-07-31 00:45:33 -0700

    Added bandpass example

--- examples/examples_3_2_8_kaiser.py
+++ examples/examples_3_2_8_kaiser.py
@@ -1,4 +1,46 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from signal_processor.finite_impulse_response import kaiser_coefficients, kaiser_filter_order, kaiser_bandpass, magnitude_response, FilterType
 
+# filter_order			nk, NK
+# passband_frequency		fp
+# passband_frequency_low	fp1, FP1
+# passband_frequency_high	fp2, FP2
+# stopband_frequency		fs, FS, f, F
+# sampling_frequency		fs, FS, f, F (CORRECTION)
+# stopband_frequency_low	fs1, fa1, FA1
+# stopband_frequency_high	fs2, fa2, FA2
+# transition_bandwidth_low	bt1
+# transition_bandwidth_high	bt2
+# transition_bandwidth		bt
+# delta				_del
+# delta_passband (ripple)	d1
+# delta_stopband (ripple)	d2
+# actual_passband_ripple	aap
+# specified_passband_ripple	ap
+# actual_stopband_attenuation	actual_minimum_stopband_attenuation
+# minimum_stopband_attenuation	specified_minimum_stopband_attenuation
+# minimum_stopband_attenuation	aa
+# parameter_d			pard
+# alpha				alp, ALP
+
+# actual_passband_ripple	aa (CORRECTION)
+# minimum_stopband_attenuation	aap (CORRECTION)
+# kaiser_coeffs			wk, WK
+# mod_bessel_fk_alpha		modified_bessel_first_kind_alpha
+# mod_bessel_fk_beta		modified_bessel_first_kind_beta
+# mod_bessel_fk_alpha		IOALP
+# mod_bessel_fk_beta		IOBE
+# beta				BE
+#				KFAC (factorial)
+# cutoff_frequency		wc, WC
+# cutoff_frequency_low		wc1, WC1
+# cutoff_frequency_high		wc2, WC2
+# initial_impulse_response	H1
+# impulse_response		H
+# sinc_function			fnsx, FNSX
+# argument			ARG
 
 # Example 5.6
 # Design a bandpass filter with the following specifications
@@ -44,3 +86,58 @@ frequency_response = [
 	(0.000, -40.95871194),
 	(0.005, -41.53972527)
 ]
+
+FREQUENCY_RESOLUTION = 500
+
+# Filter specifications
+passband_frequency_low = 120
+passband_frequency_high = 180
+specified_passband_ripple = 0.5
+stopband_frequency_low = 60
+stopband_frequency_high = 240
+minimum_stopband_attenuation = 35
+sampling_frequency = 600
+
+filter_type = FilterType.BANDPASS
+
+filter_order, delta, minimum_stopband_attenuation, parameter_d = kaiser_filter_order(filter_type,
+	passband_frequency_low,
+	passband_frequency_high,
+	stopband_frequency_low,
+	stopband_frequency_high,
+	sampling_frequency,
+	specified_passband_ripple,
+	minimum_stopband_attenuation)
+
+kaiser_coeffs, alpha = kaiser_coefficients(filter_order, minimum_stopband_attenuation)
+
+impulse_response = kaiser_bandpass(passband_frequency_low,
+	passband_frequency_high,
+	stopband_frequency_low,
+	stopband_frequency_high,
+	sampling_frequency,
+	filter_order,
+	kaiser_coeffs)
+	
+maximum_frequency = sampling_frequency / 2
+frequencies = np.linspace(0, maximum_frequency, FREQUENCY_RESOLUTION)
+angular_frequencies = frequencies * 2 * math.pi
+
+response_magnitude = [magnitude_response(w, impulse_response, sampling_frequency, filter_order)
+                      for w in angular_frequencies]
+response_magnitude_db = [20 * math.log10(max(abs(m), 1e-10)) for m in response_magnitude]
+
+# Plot
+plt.figure(figsize=(8,4))
+plt.plot(frequencies, response_magnitude_db)
+plt.xlim(0, sampling_frequency / 2)
+plt.ylim(-100, 5)
+plt.axvline(passband_frequency_low, color='green', linestyle='--', label='Passband Low')
+plt.axvline(stopband_frequency_high, color='red', linestyle='--', label='Passband High')
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Magnitude (dB)")
+plt.title(f"Kaiser Bandpass FIR (N={filter_order}, A={minimum_stopband_attenuation} dB)")
+plt.grid(True)
+plt.legend()
+plt.show()
+
