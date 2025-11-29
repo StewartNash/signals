44b9490bf193c90760fc75038bc7c83e68cd8248
stewartnash <stewart.m.nash@gmail.com>
2025-11-29 15:31:12 -0700

    Added filter class labelframe.

--- nashhertz/gui/advanced.py
+++ nashhertz/gui/advanced.py
@@ -16,6 +16,15 @@ class AdvancedFilter:
         "Matched",
         "Delay"
     ]
+    FILTER_CLASSES = [
+        "Low Pass",
+        "High Pass",
+        "Band Pass",
+        "Band Stop",
+        "Diplexer 1",
+        "Diplexer 2"
+    ]
+    FREQUENCY_SCALES = ["Rad/Sec", "Hertz"]
     def __init__(self):
         pass
 
@@ -27,16 +36,17 @@ class AdvancedForm(ttk.Frame):
             self.model = AdvancedFilter()
         self.create_form()
         
-        
     def create_form(self):
         self.left_frame = ttk.Frame(self)
         self.right_frame = ttk.Frame(self)
         
         self.left_upper_frame = ttk.Frame(self.left_frame)
         self.left_lower_frame = ttk.Frame(self.left_frame)
+        self.right_upper_frame = ttk.Frame(self.right_frame)
+        self.right_lower_frame = ttk.Frame(self.right_frame)
         
-        self.filtertype_intvar = tk.IntVar(value=2)
         self.filtertype_labelframe = tk.LabelFrame(self.left_upper_frame, text="Filter Type")
+        self.filtertype_intvar = tk.IntVar(value=2)
         for index, text in enumerate(AdvancedFilter.FILTER_TYPES):
             radio_button = tk.Radiobutton(self.filtertype_labelframe, text=text, variable=self.filtertype_intvar, value=index)
             radio_button.pack(anchor="w")
@@ -54,15 +64,54 @@ class AdvancedForm(ttk.Frame):
         self.order_stringvar = tk.StringVar()
         self.order_spinbox = tk.Spinbox(self.filterattributes_labelframe, from_=1.0, to=100.0, textvariable=self.order_stringvar)
         
+        self.filterclass_labelframe = tk.LabelFrame(self.left_lower_frame, text="Filter Class")
+        self.filterclass_intvar = tk.IntVar(value=0)
+        self.multiplebands_booleanvar = tk.BooleanVar(value=False)
+        for index, text in enumerate(AdvancedFilter.FILTER_CLASSES):
+            radio_button = tk.Radiobutton(self.filterclass_labelframe, text=text, variable=self.filterclass_intvar, value=index)
+            radio_button.pack(anchor="w")
+        self.multiplebands_checkbutton = tk.Checkbutton(self.filterclass_labelframe,
+            text="Multiple Bands",
+            command=self.generic_callback,
+            variable=self.multiplebands_booleanvar,
+            onvalue=True,
+            offvalue=False
+        )
+        
+        self.freqscale_labelframe = tk.LabelFrame(self.left_lower_frame, text="Freq Scale")
+        self.freqscale_intvar = tk.IntVar(value=1)
+        self.log_booleanvar = tk.BooleanVar(value=True)
+        for index, text in enumerate(AdvancedFilter.FREQUENCY_SCALES):
+            radio_button = tk.Radiobutton(self.freqscale_labelframe, text=text, variable=self.freqscale_intvar, value=index)
+            radio_button.pack(anchor="w")
+        self.log_checkbutton = tk.Checkbutton(self.freqscale_labelframe,
+            text="Log",
+            command=self.generic_callback,
+            variable=self.log_booleanvar,
+            onvalue=True,
+            offvalue=False
+        )
+        
+        self.graphlimits_labelframe = tk.LabelFrame(self.left_lower_frame, text="Graph Limits")
+
         self.standardattenuation_checkbutton.pack()
         self.setorder_button.pack()
         self.order_spinbox.pack()
+
+        self.multiplebands_checkbutton.pack()
+        self.log_checkbutton.pack()
         
         self.filtertype_labelframe.pack(side=tk.LEFT)
         self.filterattributes_labelframe.pack(side=tk.LEFT)
         
+        self.filterclass_labelframe.pack(side=tk.LEFT)
+        self.freqscale_labelframe.pack(side=tk.LEFT)
+        self.graphlimits_labelframe.pack(side=tk.LEFT)
+
         self.left_upper_frame.pack()
         self.left_lower_frame.pack()
+        self.right_upper_frame.pack()
+        self.right_lower_frame.pack()
 
         self.left_frame.pack(side=tk.LEFT)
         self.right_frame.pack(side=tk.LEFT)
